bf722a594bcfa34a3f66a6f6e93df2c7374f5f6d
JuanPUwu <pablys8@gmail.com>
2026-01-29 11:51:14 -0500

    antes de resposive menu

--- src/pages/Conceptualizacion/PracticarConceptualizacion.jsx
+++ src/pages/Conceptualizacion/PracticarConceptualizacion.jsx
@@ -1,3 +1,13 @@
+import "../../styles/pages.css";
+
 export default function PracticarConceptualizacion() {
-  return <div>Practicar conceptualizacion</div>;
+  return (
+    <div className="page-container">
+      <h2>Llegó la hora de practicar</h2>
+      <h5>
+        Encontrarás 4 niveles de actividades diseñadas para ayudarte a aprender
+        <br />y comprender qué es un bucle en programación.
+      </h5>
+    </div>
+  );
 }
