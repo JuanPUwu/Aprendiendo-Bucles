83f361f0b63b42d7e4a59607a3c4ada9a740f415
JuanPUwu <pablys8@gmail.com>
2026-02-21 18:47:21 -0500

    Revert "prueba de estilos camara 2.0"
    This reverts commit b85825787054f455a126d4cf40d56c7c5da34bab.

--- src/pages/ARDemo.jsx
+++ src/pages/ARDemo.jsx
@@ -1,34 +1,16 @@
 import "@ar-js-org/ar.js";
-import { useEffect } from "react";
-import "../styles/arDemo.css";
 
 export default function ARDemo() {
-  useEffect(() => {
-    document.body.classList.add("ar-demo-active");
-
-    return () => {
-      document.body.classList.remove("ar-demo-active");
-    };
-  }, []);
-
   return (
-    <div className="ar-demo">
-      <a-scene
-        embedded
-        vr-mode-ui="enabled: false"
-        device-orientation-permission-ui="enabled: false"
-        renderer="antialias: true; alpha: true"
-        arjs="sourceType: webcam; debugUIEnabled: false;"
-      >
-        <a-marker preset="hiro">
-          <a-entity
-            position="0 0 0"
-            scale="0.05 0.05 0.05"
-            gltf-model="https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-          ></a-entity>
-        </a-marker>
-        <a-entity camera></a-entity>
-      </a-scene>
-    </div>
+    <a-scene embedded arjs>
+      <a-marker preset="hiro">
+        <a-entity
+          position="0 0 0"
+          scale="0.05 0.05 0.05"
+          gltf-model="https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+        ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
   );
 }
