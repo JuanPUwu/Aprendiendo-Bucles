b657c023677f30238451caf760cf296c57532baf
JuanPUwu <pablys8@gmail.com>
2026-02-22 11:46:59 -0500

    probando si se ve el disosaurio

--- src/pages/ArCam.jsx
+++ src/pages/ArCam.jsx
@@ -7,8 +7,7 @@ export default function ArCam() {
     <div style={{ width: "100vw", height: "100vh" }}>
       <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: true;">
         <a-marker preset="hiro">
-          {/* <a-entity gltf-model="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/three.js/examples/marker-training/examples/pattern-files/dinosaur.glb"></a-entity> */}
-          <a-box color="red"></a-box>
+          <a-entity gltf-model="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/three.js/examples/marker-training/examples/pattern-files/dinosaur.glb"></a-entity>
         </a-marker>
         <a-entity camera></a-entity>
       </a-scene>
