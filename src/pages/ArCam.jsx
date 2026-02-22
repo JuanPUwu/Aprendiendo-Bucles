f38538e4b73843d98b1acdd34a84a2d37989660f
JuanPUwu <pablys8@gmail.com>
2026-02-22 11:55:51 -0500

    pruebas si muestra el gato de scratch

--- src/pages/ArCam.jsx
+++ src/pages/ArCam.jsx
@@ -7,7 +7,11 @@ export default function ArCam() {
     <div style={{ width: "100vw", height: "100vh" }}>
       <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: true;">
         <a-marker preset="hiro">
-          <a-entity gltf-model="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/three.js/examples/marker-training/examples/pattern-files/dinosaur.glb"></a-entity>
+          <a-entity
+            gltf-model="/models/scratch1.glb"
+            scale="0.5 0.5 0.5"
+            position="0 0 0"
+          ></a-entity>
         </a-marker>
         <a-entity camera></a-entity>
       </a-scene>
