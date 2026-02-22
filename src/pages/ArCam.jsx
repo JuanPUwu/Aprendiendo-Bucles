7b446866325e84fb484155a88284959ad65ed05e
JuanPUwu <pablys8@gmail.com>
2026-02-22 12:13:20 -0500

    hacer ams grande y mas iluminado

--- src/pages/ArCam.jsx
+++ src/pages/ArCam.jsx
@@ -6,17 +6,17 @@ export default function ArCam() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
-        <a-entity light="type: ambient; intensity: 1.2"></a-entity>
+        <a-entity light="type: ambient; intensity: 2"></a-entity>
 
         <a-entity
-          light="type: directional; intensity: 0.8"
+          light="type: directional; intensity: 2"
           position="1 2 1"
         ></a-entity>
 
         <a-marker preset="hiro">
           <a-entity
             gltf-model="/models/scratch1.glb"
-            scale="0.5 0.5 0.5"
+            scale="2 2 2"
             position="0 0 0"
           ></a-entity>
         </a-marker>
