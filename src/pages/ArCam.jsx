0af1d713d67e6b5105f0014f5551eca49f644f5d
JuanPUwu <pablys8@gmail.com>
2026-02-22 11:40:10 -0500

    prueba hiro con correcciones de css

--- src/pages/ArCam.jsx
+++ src/pages/ArCam.jsx
@@ -6,13 +6,10 @@ export default function ArCam() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: true;">
-        {/* 🟥 MARCADOR POR DEFECTO (HIRO) */}
         <a-marker preset="hiro">
-          {/* 🧱 TU MODELO 3D PERSONALIZADO */}
-          <a-entity gltf-model="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/three.js/examples/marker-training/examples/pattern-files/dinosaur.glb"></a-entity>
+          {/* <a-entity gltf-model="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/three.js/examples/marker-training/examples/pattern-files/dinosaur.glb"></a-entity> */}
+          <a-box color="red"></a-box>
         </a-marker>
-
-        {/* 📷 Cámara */}
         <a-entity camera></a-entity>
       </a-scene>
     </div>
