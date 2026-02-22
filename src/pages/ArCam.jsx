52cc8bd6df23bf6bdc89b8b1309c16ea2bbe2b2b
JuanPUwu <pablys8@gmail.com>
2026-02-22 15:49:29 -0500

    modelos hasta la g, antes de pruebas

--- src/pages/ArCam.jsx
+++ src/pages/ArCam.jsx
@@ -28,6 +28,41 @@ export default function ArCam() {
           ></a-entity>
         </a-marker>
 
+        <a-marker type="pattern" url="/markers/pattern-marker-c.patt">
+          <a-entity
+            gltf-model="/models/scratch-c.glb"
+            scale="4 0.5 2"
+          ></a-entity>
+        </a-marker>
+
+        <a-marker type="pattern" url="/markers/pattern-marker-d.patt">
+          <a-entity
+            gltf-model="/models/scratch-d.glb"
+            scale="4 0.5 2"
+          ></a-entity>
+        </a-marker>
+
+        <a-marker type="pattern" url="/markers/pattern-marker-e.patt">
+          <a-entity
+            gltf-model="/models/scratch-e.glb"
+            scale="4 0.5 2"
+          ></a-entity>
+        </a-marker>
+
+        <a-marker type="pattern" url="/markers/pattern-marker-f.patt">
+          <a-entity
+            gltf-model="/models/scratch-f.glb"
+            scale="4 0.5 2"
+          ></a-entity>
+        </a-marker>
+
+        <a-marker type="pattern" url="/markers/pattern-marker-g.patt">
+          <a-entity
+            gltf-model="/models/scratch-g.glb"
+            scale="4 0.5 2"
+          ></a-entity>
+        </a-marker>
+
         <a-entity camera></a-entity>
       </a-scene>
     </div>
