7af2c8f97d666e7cfbdfa56899669a2745adf939
JuanPUwu <pablys8@gmail.com>
2026-02-22 13:35:11 -0500

    corregir todos los modelos y ajustar iluminacion

--- src/pages/ArCam.jsx
+++ src/pages/ArCam.jsx
@@ -10,9 +10,9 @@ export default function ArCam() {
         arjs="sourceType: webcam; debugUIEnabled: true;"
         light="defaultLightsEnabled: false"
       >
-        <a-light type="ambient" intensity="1.3"></a-light>
+        <a-light type="ambient" intensity="2"></a-light>
 
-        <a-light type="directional" position="0 2 2" intensity="1"></a-light>
+        <a-light type="directional" position="0 2 2" intensity="1.8"></a-light>
 
         <a-marker preset="hiro">
           <a-entity gltf-model="/models/scratch2.glb" scale="2 2 2"></a-entity>
