3a71bf1cf116bcd87d6b38bdd4c8ed9d92d71107
JuanPUwu <pablys8@gmail.com>
2026-02-21 19:02:28 -0500

    integrar ar-cam a la seccion

--- src/pages/ArCam.jsx
+++ src/pages/ArCam.jsx
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+
 export default function ArCam() {
-  return <>ar cam</>;
+  useEffect(() => {}, []);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
+        {/* Cámara AR */}
+        <a-marker preset="hiro">
+          <a-box position="0 0.5 0" material="color: red"></a-box>
+        </a-marker>
+
+        <a-entity camera></a-entity>
+      </a-scene>
+    </div>
+  );
 }
